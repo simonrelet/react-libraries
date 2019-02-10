ee747e082048dc7232e8ae98f57670d3a8a41b00
Simon Relet <simon.relet@gmail.com>
2019-02-10 15:29:03 +0100

    remove react-behave

--- packages/react-behave/src/core/index.js
+++
@@ -1,4 +0,0 @@
-export { default as getDisplayName } from './getDisplayName'
-export { default as getOtherProps } from './getOtherProps'
-export { default as getScreenSize } from './getScreenSize'
-export { default as watchResize } from './watchResize'
